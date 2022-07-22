86501a1277e94183388a10ffef317abebe284cc7
Jason Kwak <ralph9696@naver.com>
2022-07-22 16:26:46 -0400

    commit tree cb5dc034658965079b6d023afe50249138e29624

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  let greeting
+  if (someNumber <= 400){
+    greeting = `This one is on me!`
+  }
+  else if (someNumber <= 2000){
+    greeting = `That will be twenty bucks.`
+  }
+  else if (someNumber > 2000 && someNumber <= 2500) {
+    greeting = `I will gladly take your thirty bucks.`
+  }
+  else if (someNumber > 2500) {
+    greeting =`No can do.`
+  }
+  return greeting
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const response = city === `NYC` ? `Ok, sounds good.` : `No go.`
+  return response
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(amount){
   // Write your code here!
+  let tipNote
+  switch (amount) {
+    case `generous`:
+      tipNote = `Thank you so much.`;
+      break;
+    case `not as generous`:
+      tipNote = `Thank you.`;
+      break;
+    default:
+      tipNote = `Bye.`;
+      break;
+  }
+  return tipNote
 }
